f4ac9508fd29139e3a2330ac09b76c7d8c5236c5
Lexa622 <lexa622@gmail.com>
2024-10-14 23:40:21 +0300

    Домашнее задание по теме "CRUD Запросы: Get, Post, Put Delete.

--- module_16_3.py
+++ module_16_3.py
@@ -12,7 +12,7 @@ async def get_all_users() -> dict:
 @app.post("/user/{username}/{age}")             # создать пользователя
 async def create_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username",
                                                     example="Lexar")],
-                      age: int = Path(ge=1, le=100, description="Enter User ID", example=78)) -> str:
+                      age: int = Path(ge=18, le=120, description="Enter age", example=53)) -> str:
     user_id = str(int(max(users, key=int))+1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
@@ -31,20 +31,3 @@ async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="En
 async def delete_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", example=78)) -> str:
     users.pop(str(user_id))
     return f"User {user_id} has been deleted"
-"""1. GET '/users'
-{
-"1": "Имя: Example, возраст: 18"
-}
-2. POST '/user/{username}/{age}' # username - UrbanUser, age - 24
-"User 2 is registered"
-3. POST '/user/{username}/{age}' # username - NewUser, age - 22
-"User 3 is registered"
-4. PUT '/user/{user_id}/{username}/{age}' # user_id - 1, username - UrbanProfi, age - 28
-"User 1 has been updated"
-5. DELETE '/user/{user_id}' # user_id - 2
-"User 2 has been deleted"
-6. GET '/users'
-{
-"1": "Имя: UrbanProfi, возраст: 28",
-"3": "Имя: NewUser, возраст: 22"
-}"""
