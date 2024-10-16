169c001fe86db033b0519c1314eafbbf48ff7303
Lexa622 <lexa622@gmail.com>
2024-10-16 20:10:58 +0300

    Домашнее задание по теме "Модели данных Pydantic"

---
+++ module_16_4.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+app = FastAPI()
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+@app.get("/users")                             # получить всех пользователуй
+async def get_all_users() -> List[User]:
+    return users
+
+
+@app.post("/user/{username}/{age}")             # создать пользователя
+async def create_user(user: User, username: str, age: int):
+    if len(users) == 0:
+        user.id = 1
+    else:
+        user.id = users[len(users) - 1].id + 1
+    user.username = username
+    user.age = age
+    users.append(user)
+    return user
+
+
+@app.put("/user/{user_id}/{username}/{age}")    # обновление пользователя
+async def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete("/user/{user_id}")                   # удалить пользователя
+async def delete_user(user_id: int):
+    for i in range(len(users)):
+        if users[i].id == user_id:
+            user = users[i]
+            users.pop(i)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
